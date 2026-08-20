947ab6e5c0cb73e241e4e3ef0b30f2865a61eb8a
Mourad <mouradface1998@gmail.com>
2026-08-20 14:49:57 +0100

    save

--- pair-trading/dataProvider.py
+++ pair-trading/dataProvider.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-def get_df(before="'2020-01-01'"):
+def get_df(timeframe="m15", before="'2020-01-01'"):
 
-    a_df = pd.read_csv(BASE_DIR / "gbpusd-h1-bid-2017-01-01-2026-08-18.csv")
-    b_df = pd.read_csv(BASE_DIR / "eurusd-h1-bid-2017-01-01-2026-08-18.csv")
+    a_df = pd.read_csv(BASE_DIR / f"gbpusd-{timeframe}.csv")
+    b_df = pd.read_csv(BASE_DIR / f"eurusd-{timeframe}.csv")
 
     a_df.set_index("Datetime", inplace=True)
     b_df.set_index("Datetime", inplace=True)
